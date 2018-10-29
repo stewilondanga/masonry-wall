f291f319bc35643b7e0f30fb7dae8c4181b45938
Stewart ilondanga <stilondanga@gmail.com>
2018-10-29 09:36:37 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*var navigate = (function() {
+var navigate = (function() {
   $('.dd').toggle();
   $('.dd_btn').click(function() {
     var dataName = $(this).attr('data-name');
@@ -7,7 +7,7 @@
   });
 })();
 
-$('.grid').scripts({
+/*$('.grid').scripts({
   itemSelector: '.grid-item',
   columnWidth: 160
 });
